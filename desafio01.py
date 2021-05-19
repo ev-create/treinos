f9e4b96010c1727173a45fa804485509da180318
ev-create <84459035+ev-create@users.noreply.github.com>
2021-05-19 11:22:37 -0300

    Add files via upload

---
+++ desafio01.py
@@ -0,0 +1,11 @@
+nome = input('Qual é o seu nome?')
+print('Olá',nome,'!', 'Prazer em te Conhecer!')
+diaDeNasc = input('Em que Dia você Nasceu? ')
+mesDeNasc = input('Em Que Mês você Nasceu? ')
+anoDeNasc = input('Em Que Ano você Nasceu? ')
+print ('Você Nasceu',diaDeNasc, mesDeNasc, anoDeNasc,'correto?')
+pnum = input('Digite o Primeiro Numero ')
+snum = input('Digite o Segundo Número ')
+print (pnum * snum)
+
+
